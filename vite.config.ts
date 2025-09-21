9b4ecf9c5cf0baf56334de1706f531febc8e74b5
bhushanpawar <bhushanpawarj@gmail.com>
2025-09-21 16:46:06 +0530

    updated the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()].filter(Boolean),
+  base: "/shivar-solar-energy/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
